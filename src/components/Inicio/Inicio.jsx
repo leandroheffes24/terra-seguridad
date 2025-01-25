d633da8a6a7f5b981425c315a03338b03cdbcdd1
leandroheffes24 <leandroheffes@outlook.es>
2025-01-25 16:38:52 -0300

    inicio complete, missing form funcionality

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -9,9 +9,33 @@ export default function Inicio(){
 
             <div className={styles.formContainer}>
                 <div className={styles.formTextContainer}>
-                    <p className={styles.formTopText}>OBTENÉ UNA ASESORÍA PERSONALIZADA TOTALMENTE GRATIS.</p>
+                    <p className={styles.formTopText}>ASESORÍA PERSONALIZADA GRATIS</p>
                     <p className={styles.formBottomText}>DESCUBRÍ LO QUE NECESITAS PARA GARANTIZAR SEGURIDAD Y TRANQUILIDAD A TU VIDA.</p>
                 </div>
+                <form className={styles.form} action="">
+                    <div className={styles.formLabelAndInputContainer}>
+                        <label htmlFor="nombre">Nombre</label>
+                        <input type="text" id='nombre' name='nombre' placeholder='Tu nombre' required />
+                    </div>
+                    <div className={styles.formLabelAndInputContainer}>
+                        <label htmlFor="apellido">Apellido</label>
+                        <input type="text" id='apellido' name='apellido' placeholder='Tu apellido' required />
+                    </div>
+                    <div className={styles.formLabelAndInputContainer}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id='email' name='email' placeholder='Tu email' required />
+                    </div>
+                    <div className={`${styles.formLabelAndInputContainer} ${styles.areaCodeContainer}`}>
+                        <label htmlFor="codigo">Cod. de área</label>
+                        <input type="number" id='codigo' name='codigo' placeholder='Sin 15. Ej: 11' required />
+                    </div>
+                    <div className={`${styles.formLabelAndInputContainer} ${styles.phoneContainer}`}>
+                        <label htmlFor="telefono">Teléfono</label>
+                        <input type="number" id='telefono' name='telefono' placeholder='Nro. de teléfono' required />
+                    </div>
+
+                    <button type='submit' className={styles.formSubmitButton}>Solicitar asesoría gratuita</button>
+                </form>
             </div>
         </section>
     )
