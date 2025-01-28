961c5784f4e41dbb8dfad45f05c9e4a817feb96d
leandroheffes24 <leandroheffes@outlook.es>
2025-01-28 11:07:08 -0300

    servicios y equipos for mobile in progress

--- src/components/ServiciosYEquipos/ServiciosYEquipos.jsx
+++ src/components/ServiciosYEquipos/ServiciosYEquipos.jsx
@@ -3,10 +3,11 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 import homeImage from '../../assets/kit1.webp'
 import qolsys from '../../assets/qolsys.webp'
+import { scrollToSection } from '../../utils/scrollToSection';
 
 export default function ServiciosYEquipos(){
     return(
-        <section id='servicios-y-equipos'>
+        <section className={styles.serviciosYEquiposSection} id='servicios-y-equipos'>
             <div className={styles.titleAndSubtitleContainer}>
                 <h2>Servicios y equipos disponibles</h2>
                 <h3>Nuestros equipos para tu tranquilidad.</h3>
@@ -35,31 +36,38 @@ export default function ServiciosYEquipos(){
                     type: 'loop',
                     perPage: 1,
                     perMove: 1,
-                    // gap: '2rem',
+                    gap: '2rem',
                     // padding: '30px',
-                    pagination: false
+                    pagination: false,
                 }}
             >
                 <SplideSlide className={styles.card}>
-                    <div className={styles.imageAndTitleContainer}>
-                        <img className={styles.cardImage} src={homeImage} alt="Kit para hogares ilustrativa" />
-                        <h4 className={styles.cardTitle}>Servicio de seguridad para tu hogar</h4>
-                    </div>
+                    <div className={styles.container}>
+                        <div className={styles.imageContainer}>
+                            <img className={styles.cardImage} src={homeImage} alt="Kit para hogares ilustrativa" />
+                        </div>
 
-                    <ul className={styles.cardList}>
-                        <li className={styles.itemList}>Kit de alarma completo.</li>
-                        <li className={styles.itemList}>Servicio inmediato con policía, bomberos y ambulancia.</li>
-                        <li className={styles.itemList}>Opción de agregar una cámara sin costo extra.</li>
-                        <li className={styles.itemList}>Soporte las 24 horas del día, los 7 días de la semana.</li>
-                        <li className={styles.itemList}>Control total desde la aplicación.</li>
-                    </ul>
+                        <div className={styles.infoContainer}>
+                            <h4 className={styles.cardTitle}>Servicio de seguridad para tu hogar</h4>
 
-                    <div className={styles.kitImageContainer}>
-                        <img className={styles.kitImage} src={qolsys} alt="Imágen Qolsys" />
-                    </div>
+                            <ul className={styles.cardList}>
+                                <li className={styles.cardItemList}>Kit de alarma completo.</li>
+                                <li className={styles.cardItemList}>Servicio inmediato con policía, bomberos y ambulancia.</li>
+                                <li className={styles.cardItemList}>Opción de agregar una cámara sin costo extra.</li>
+                                <li className={styles.cardItemList}>Soporte las 24 horas del día, los 7 días de la semana.</li>
+                                <li className={styles.cardItemList}>Control total desde la aplicación.</li>
+                            </ul>
 
-                    <div className={styles.contactButtonContainer}>
-                        <a className={styles.contactButton} href="#contacto">Conocé más</a>
+                            <div className={styles.kitImageContainer}>
+                                <img className={styles.kitImage} src={qolsys} alt="Imágen Qolsys" />
+                            </div>
+
+                            <div className={styles.contactButtonContainer}>
+                                <button className={styles.contactButton} >
+                                    <a className={styles.contactLink} href="#contacto" onClick={scrollToSection}>Conocé más</a>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </SplideSlide>
             </Splide>
