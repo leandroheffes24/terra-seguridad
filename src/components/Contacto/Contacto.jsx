91bd39889662c4532779c15a1ba487185a0726f2
leandroheffes24 <leandroheffes@outlook.es>
2025-01-30 08:26:17 -0300

    contact section complete, missing form funcion

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -15,28 +15,28 @@ export default function Contacto(){
 
                 <div className={styles.formInfoContainer}>
                     <div className={styles.labelAndInputContainer}>
-                        <label htmlFor="nombre">Nombre</label>
-                        <input type="text" id='nombre' name='nombre' placeholder='Tu nombre' />
+                        <label htmlFor="nombreContacto">Nombre</label>
+                        <input type="text" id='nombreContacto' name='nombre' placeholder='Tu nombre' maxLength={30} />
                     </div>
 
                     <div className={styles.labelAndInputContainer}>
-                        <label htmlFor="apellido">Apellido</label>
-                        <input type="text" id='apellido' name='apellido' placeholder='Tu apellido' />
+                        <label htmlFor="apellidoContacto">Apellido</label>
+                        <input type="text" id='apellidoContacto' name='apellido' placeholder='Tu apellido' maxLength={30} />
                     </div>
 
                     <div className={styles.labelAndInputContainer}>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id='email' name='email' placeholder='Tu email' />
+                        <label htmlFor="emailContacto">Email</label>
+                        <input type="email" id='emailContacto' name='email' placeholder='Tu email' maxLength={80} />
                     </div>
 
                     <div className={`${styles.labelAndInputContainer} ${styles.codigoContainer}`}>
-                        <label htmlFor="codigo">Cod. de área</label>
-                        <input type="number" id='codigo' name='codigo' placeholder='Sin 15. Ej: 11' />
+                        <label htmlFor="codigoContacto">Cod. de área</label>
+                        <input type="number" id='codigoContacto' name='codigo' placeholder='Sin 15. Ej: 11' />
                     </div>
 
                     <div className={`${styles.labelAndInputContainer} ${styles.telefonoContainer}`}>
-                        <label htmlFor="telefono">Teléfono</label>
-                        <input type="number" id='telefono' name='telefono' placeholder='Ej: 1234-5678' />
+                        <label htmlFor="telefonoContacto">Teléfono</label>
+                        <input type="number" id='telefonoContacto' name='telefono' placeholder='Ej: 1234-5678' />
                     </div>
 
                     <button className={styles.submitButton} type='submit'>Enviar</button>
